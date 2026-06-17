6a97fc2f29b61a2b7ef7cde78691c38a7d9a23a9
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2026-06-17 11:27:13 +0100

    adds lots more low-level categorical machinery

--- IUTPproto.hs
+++ IUTPproto.hs
@@ -9,18 +9,24 @@ data Category obj
       , morphisms :: Set (obj,obj) }
 -- morphisms should be minimal? maximal?
 
-type Signature = Set String
+apply :: Eq obj => Set (obj,obj) -> obj -> obj
+apply m o 
+  = search o $ S.toList m
+  where
+    search o [] = o -- always an identity arrow
+    search o ((d,r):rest)
+      | o == d  =  r
+      | otherwise  =  search o rest
 
+data Fun dom rng -- Functor defined in Prelude
+  = F { objf :: Set (dom,rng)
+      , arrf :: Set ((dom,dom),(rng,rng)) }
 
-
-newtype SET set = SET set -- the ultimate abstraction ;-)
-newtype CAT cat = CAT cat
-
-data Institution sig set cat
+data Institution sig sent model
   = I { category :: Category sig
-      , sentencef :: sig -> Category set
-      , modelf :: sig -> CAT cat -- use comorphisms, rather than morphisms
-      , satrel :: Set (CAT cat,SET set)
+      , sentencef :: Fun sig sent
+      , modelf :: Fun sig model -- use comorphisms, rather than morphisms
+      , satrel :: Set (model,sent)
       -- derived:
       , comorphisms :: Set (sig,sig)
       }
@@ -28,7 +34,13 @@ data Institution sig set cat
 iDerive :: Ord sig => Institution sig set cat -> Institution sig set cat
 iDerive inst = inst{ comorphisms = S.map swap (morphisms (category inst))}
 
-sat :: (Ord (CAT cat), Ord (SET set)) 
-    => Institution sig set cat -> CAT cat -> SET set -> Bool
-sat inst catobj setobj
-  = (catobj,setobj) `S.member` satrel inst
+iSen inst sig = apply (objf (sentencef inst)) sig
+
+sat :: (Ord sent, Ord model) 
+    => Institution sig sent model -> model -> sent -> Bool
+sat inst modobj sentobj
+  = (modobj,sentobj) `S.member` satrel inst
+
+type Signature = Set String
+
+type Sentence = [String] 
