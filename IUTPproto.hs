130bdc1b092a9e57dc943b8e5d796b844bb98bd1
Andrew Butterfield <Andrew.Butterfield@scss.tcd.ie>
2026-06-17 17:46:56 +0100

    now distinguish morphism from general function

--- IUTPproto.hs
+++ IUTPproto.hs
@@ -9,8 +9,8 @@ data Category obj
       , morphisms :: Set (obj,obj) }
 -- morphisms should be minimal? maximal?
 
-apply :: Eq obj => Set (obj,obj) -> obj -> obj
-apply m o 
+morph :: Eq obj => Set (obj,obj) -> obj -> obj
+morph m o 
   = search o $ S.toList m
   where
     search o [] = o -- always an identity arrow
@@ -21,6 +21,15 @@ apply m o
 data Fun dom rng -- Functor defined in Prelude
   = F { objf :: Set (dom,rng)
       , arrf :: Set ((dom,dom),(rng,rng)) }
+
+apply :: Eq dom => Set (dom,mrg) -> dom -> mrg
+apply f x 
+  = search x $ S.toList f
+  where
+    search x [] = error ""
+    search x ((d,r):rest)
+      | x == d  =  r
+      | otherwise  =  search x rest
 
 data Institution sig sent model
   = I { category :: Category sig
@@ -34,8 +43,10 @@ data Institution sig sent model
 iDerive :: Ord sig => Institution sig set cat -> Institution sig set cat
 iDerive inst = inst{ comorphisms = S.map swap (morphisms (category inst))}
 
-iSen :: Eq obj => Institution obj obj model -> obj -> obj
+iSen :: Eq sig => Institution sig sent model -> sig -> sent
 iSen inst sig = apply (objf (sentencef inst)) sig
+
+-- iMod :: Eq obj => Institution obj 
 
 sat :: (Ord sent, Ord model) 
     => Institution sig sent model -> model -> sent -> Bool
